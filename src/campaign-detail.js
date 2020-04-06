14d44a2f37f71e672d790a219a5f3a5918c135b7
CadiePerkins <56050655+CadiePerkins@users.noreply.github.com>
2020-04-06 11:21:20 -0700

    some test campaigns

--- src/campaign-detail.js
+++ src/campaign-detail.js
@@ -1,82 +1,35 @@
-import React, { useState } from 'react';
-
 import { useRouteMatch } from 'react-router-dom';
-import { useHistory } from 'react-router-dom';
 import * as bs from 'react-bootstrap';
-import AppContext from './context'
 
-// export default function ProductDetail(props) {
-//     const match = useRouteMatch("/product/:id")
-//     const context = React.useContext(AppContext)
+export default function ProductDetail(props) {
+    const match = useRouteMatch("/campaign/:campaign")
+    //const context = React.useContext(AppContext)
 
-//     const product = context.products.find(({ id }) => id === parseInt(match.params.id))
-    
-//     const [num, setImg] = useState('-1')
+    const campaigns = context.products.find(({ id }) => id === parseInt(match.params.id))
 
-//     const history = useHistory()
 
-//     if (product == null) {
-//         return (
-//             <bs.Container>
-//                 Item not found.
-//             </bs.Container>
-//         )
-//     }
-//     else {
-//         return (
-//             <bs.Container>
-//                 <bs.Row>
-//                     <bs.Col md="8" className="my-4">
-//                         <h2>{product.name}</h2>
-//                         <h4>${product.price}</h4>
-//                         <p>{product.description}</p>
-//                         <br></br>
-//                         <bs.Button 
-//                             to={"/cart"}
-//                             variant="primary" 
-//                             onClick={e => {
-//                                 context.addToCart(match.params.id)
-//                                 history.push("/cart")
-//                             }}>
-//                                 Add to Cart
-//                         </bs.Button>
-//                     </bs.Col>
-//                     <bs.Col md="4">
-//                         <bs.Image
-//                             src={"/products/" + product.filename + num + ".png"}
-//                             className="border float-right my-4"
-//                             style={{ width: "300px", height: "300px" }}>
-//                         </bs.Image>
-//                         <bs.Image
-//                             src={"/products/" + product.filename + "-1.png"}
-//                             className="border my-1"
-//                             style={{ width: "30px", height: "30px" }}
-//                             onMouseEnter={() => setImg('-1')}>
-//                         </bs.Image>
-//                         <bs.Image
-//                             src={"/products/" + product.filename + "-2.png"}
-//                             className="border my-1 mx-2"
-//                             style={{ width: "30px", height: "30px" }}
-//                             onMouseEnter={() => setImg('-2')}>
-//                         </bs.Image>
-//                         <bs.Image
-//                             src={"/products/" + product.filename + "-3.png"}
-//                             className="border my-1"
-//                             style={{ width: "30px", height: "30px" }}
-//                             onMouseEnter={() => setImg('-3')}>
-//                         </bs.Image>
-//                         <bs.Image
-//                             src={"/products/" + product.filename + "-4.png"}
-//                             className="border my-1 mx-2"
-//                             style={{ width: "30px", height: "30px" }}
-//                             onMouseEnter={() => setImg('-4')}>
-//                         </bs.Image>
-//                     </bs.Col>
-//                 </bs.Row>
-//                 <bs.Row>
+    if (campaigns == null) {
+        return (
+            <bs.Container>
+                Campaign not found.
+            </bs.Container>
+        )
+    }
+    else {
+        return (
+            <bs.Container>
+                <bs.Row>
+                    <bs.Col md="8" className="my-4">
+                        
+                    </bs.Col>
+                    <bs.Col md="4">
+                        
+                    </bs.Col>
+                </bs.Row>
+                <bs.Row>
 
-//                 </bs.Row>
-//             </bs.Container>
-//         )
-//     }
-// }
+                </bs.Row>
+            </bs.Container>
+        )
+    }
+}
