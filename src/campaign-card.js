90bd7cae704dbc49f42144f98fe5938d57b3ce51
Mitchell Johnson <mitchelljohnson@Mitchells-MacBook-Pro-3.local>
2020-04-06 15:33:38 -0600

    Switched some things from Product to Campaign

---
+++ src/campaign-card.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import * as bs from 'react-bootstrap'
+import {BrowserRouter as  Route,Link } from "react-router-dom"
+
+function CampaignCard(campaign){
+  let name = campaign.campaign.name.replace(" ", '_');
+  let lower = name.toLowerCase()
+return(
+  <bs.Card style={{ width: '10rem', backgroundColor: "gray" }}>
+ 
+    <bs.Card.Link href={"/campaign/" +campaign.campaign.id}  style = {{position: "absolute", top: "0px", right: "3px"}}>Detail</bs.Card.Link>
+  <Route></Route>
+  <bs.Card.Body>
+    <bs.Card.Title>{campaign.campaign.title}</bs.Card.Title>
+    <bs.Card.Text>
+      {campaign.campaign.current_amount}/{campaign.campaign.goal}
+    </bs.Card.Text>
+    <Link className="btn btn-primary" to={"/campaign/"+campaign.campaign.campaign_id}>
+    {/*<bs.Button variant="primary">Check it out</bs.Button>*/}
+    check it out
+    </Link>
+    
+  </bs.Card.Body>
+</bs.Card>
+  
+
+)
+}
+
+export default CampaignCard;
--- src/home.js
+++ src/home.js
@@ -1,43 +1,42 @@
 import React from 'react'
 import * as bs from 'react-bootstrap'
 //import PRODUCTS from './product_images/src/products'
-import ProductCard from './ProductCard'
+import CampaignCard from './campaign-card'
 import { useParams } from 'react-router-dom';
 import AppContext from './context';
 
 
-function organize(name, products){
-    //let products = Object.values(PRODUCTS);
+function organize(name, campaigns){
     //if it isn't null, then filter out anything not in the category
     if(name != null){
-        products = products.filter((val)=> val.category.title === name)
+        campaigns = campaigns.filter((val)=> val.category.title === name)
     }
 
-    let productHolder = [];
-    //first loop is going through every product
-    for(let i = 0; i < products.length; i+=4){
+    let campaignHolder = [];
+    //first loop is going through every campaign
+    for(let i = 0; i < campaigns.length; i+=4){
         let fourSlots = [];
-        //breaking up the products into groups of 4
-        for(let j = i; j < i+4 && j <products.length; j++){
-            fourSlots.push(products[j]);
+        //breaking up the campaigns into groups of 4
+        for(let j = i; j < i+4 && j <campaigns.length; j++){
+            fourSlots.push(campaigns[j]);
         }
-        productHolder.push(fourSlots);
+        campaignHolder.push(fourSlots);
     }
-    return productHolder;
+    return campaignHolder;
 }
 
 
 function Home(props){
     
     const context = React.useContext(AppContext);
-    let products = []
-    for(let i = 1; i < (Object.keys(context.products).length) +1; i++){
-        products.push(context.products[i])
+    let campaigns = []
+    for(let i = 1; i < (Object.keys(context.campaigns).length) +1; i++){
+        campaigns.push(context.campaigns[i])
     }
     
     let{category} = useParams();
     const [sort, setSort] = useState(1);
-   let organizedProducts = organize(category, products);
+   let organizedCampaigns = organize(category, campaigns);
 
    let count = 0;
 return(
@@ -47,24 +46,24 @@ return(
             <div className="text-center mt-5">
             {/* 
                 list the objects, but make sure that we don't try and make a card for 
-                a product that isn't there.
+                a campaign that isn't there.
             */}
-            {Object.values(organizedProducts).map((product) => {
-                return(<bs.ListGroup  key={product[0]+count++}  horizontal>
+            {Object.values(organizedCampaigns).map((campaign) => {
+                return(<bs.ListGroup  key={campaign[0]+count++}  horizontal>
                   <bs.Col md="3">
-                    <bs.ListGroup.Item sm={3} key={product[0].id} style={{ backgroundColor: "lightblue"}}> <ProductCard product={product[0]}/> </bs.ListGroup.Item>
+                    <bs.ListGroup.Item sm={3} key={campaign[0].id} style={{ backgroundColor: "lightblue"}}> <CampaignCard campaign={campaign[0]}/> </bs.ListGroup.Item>
                     </bs.Col>
-                    {product.length > 1?
+                    {campaign.length > 1?
                     (<bs.Col md="3">
-                    <bs.ListGroup.Item sm={3} key={product[1].id} style={{ backgroundColor: "lightblue"}}><ProductCard product={product[1]}/></bs.ListGroup.Item>
+                    <bs.ListGroup.Item sm={3} key={campaign[1].id} style={{ backgroundColor: "lightblue"}}><CampaignCard campaign={campaign[1]}/></bs.ListGroup.Item>
                     </bs.Col>):<div></div>}
-                    {product.length >2?
+                    {campaign.length >2?
                     (<bs.Col md="3">
-                    <bs.ListGroup.Item sm={3} key={product[2].id} style={{ backgroundColor: "lightblue"}}><ProductCard product={product[2]}/></bs.ListGroup.Item>
+                    <bs.ListGroup.Item sm={3} key={campaign[2].id} style={{ backgroundColor: "lightblue"}}><CampaignCard campaign={campaign[2]}/></bs.ListGroup.Item>
                     </bs.Col>):<div></div>}
-                    {product.length >3?
+                    {campaign.length >3?
                     (<bs.Col md="3">
-                        <bs.ListGroup.Item sm={3} key={product[3].id} style={{ backgroundColor: "lightblue"}}><ProductCard product={product[3]}/></bs.ListGroup.Item>
+                        <bs.ListGroup.Item sm={3} key={campaign[3].id} style={{ backgroundColor: "lightblue"}}><CampaignCard campaign={campaign[3]}/></bs.ListGroup.Item>
                     </bs.Col>):<div></div>}
                   </bs.ListGroup>)})}
             </div>
