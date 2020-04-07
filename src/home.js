5fa89aa1881971606ad736a4fe006a01cd328f62
Mitchell Johnson <mitchelljohnson@Mitchells-MacBook-Pro-3.local>
2020-04-07 13:09:55 -0600

    changed it so it's not backend reliant

--- src/home.js
+++ src/home.js
@@ -11,8 +11,14 @@ function organize(name, campaigns) {
     if (name != null) {
         campaigns = campaigns.filter((val) => val.category.title === name)
     }
-
+   /* if(charitySort==1){
+        campaigns = campaigns.filter((val) => val.is_charity===1)
+    }
+    else if(charitySort==2){
+        campaigns = campaigns.filter((val) => val.is_charity===2)
+    }*/
     let campaignHolder = [];
+    console.log("the campaigns:", campaigns);
     //first loop is going through every campaign
     for (let i = 0; i < campaigns.length; i += 4) {
         let fourSlots = [];
@@ -26,18 +32,21 @@ function organize(name, campaigns) {
 }
 
 
+
 function Home(props) {
 
     const context = React.useContext(AppContext);
     let campaigns = []
+    const [charitySort, setCharitySort] = useState(0);
+    console.log("Why is there no context?", context.campaigns);
     for (let i = 1; i < (Object.keys(context.campaigns).length) + 1; i++) {
-        campaigns.push(context.campaigns[i])
+        campaigns.push(context.campaigns[i-1])
     }
-
+    console.log("what is it? ", campaigns);
     let { category } = useParams();
-    const [sort, setSort] = useState(1);
+    
     let organizedCampaigns = organize(category, campaigns);
-
+    console.log("what do we have here?",organizedCampaigns);
     let count = 0;
     return (
         <bs.Container fluid className="p-0">
@@ -51,19 +60,19 @@ function Home(props) {
                     {Object.values(organizedCampaigns).map((campaign) => {
                         return (<bs.ListGroup key={campaign[0] + count++} horizontal>
                             <bs.Col md="3">
-                                <bs.ListGroup.Item sm={3} key={campaign[0].id} style={{ backgroundColor: "lightblue" }}> <CampaignCard campaign={campaign[0]} /> </bs.ListGroup.Item>
+                                <bs.ListGroup.Item sm={3} key={campaign[0].campaign_id} style={{ backgroundColor: "lightblue" }}> <CampaignCard campaign={campaign[0]} /> </bs.ListGroup.Item>
                             </bs.Col>
                             {campaign.length > 1 ?
                                 (<bs.Col md="3">
-                                    <bs.ListGroup.Item sm={3} key={campaign[1].id} style={{ backgroundColor: "lightblue" }}><CampaignCard campaign={campaign[1]} /></bs.ListGroup.Item>
+                                    <bs.ListGroup.Item sm={3} key={campaign[1].campaign_id} style={{ backgroundColor: "lightblue" }}><CampaignCard campaign={campaign[1]} /></bs.ListGroup.Item>
                                 </bs.Col>) : <div></div>}
                             {campaign.length > 2 ?
                                 (<bs.Col md="3">
-                                    <bs.ListGroup.Item sm={3} key={campaign[2].id} style={{ backgroundColor: "lightblue" }}><CampaignCard campaign={campaign[2]} /></bs.ListGroup.Item>
+                                    <bs.ListGroup.Item sm={3} key={campaign[2].campaign_id} style={{ backgroundColor: "lightblue" }}><CampaignCard campaign={campaign[2]} /></bs.ListGroup.Item>
                                 </bs.Col>) : <div></div>}
                             {campaign.length > 3 ?
                                 (<bs.Col md="3">
-                                    <bs.ListGroup.Item sm={3} key={campaign[3].id} style={{ backgroundColor: "lightblue" }}><CampaignCard campaign={campaign[3]} /></bs.ListGroup.Item>
+                                    <bs.ListGroup.Item sm={3} key={campaign[3].campaign_id} style={{ backgroundColor: "lightblue" }}><CampaignCard campaign={campaign[3]} /></bs.ListGroup.Item>
                                 </bs.Col>) : <div></div>}
                         </bs.ListGroup>)
                     })}
