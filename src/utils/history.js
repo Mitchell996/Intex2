b6a52f251566d6d97daa520eee5e725089963218
CadiePerkins <56050655+CadiePerkins@users.noreply.github.com>
2020-04-07 16:01:29 -0700

    authentication stuff

---
+++ src/utils/history.js
@@ -0,0 +1,7 @@
+// import createHistory from 'history/createBrowserHistory';
+
+// export default createHistory({
+//   basename: process.env.NODE_ENV === 'development' ? '' : '/reactivesearch-auth0-example'
+// });
+import { createBrowserHistory } from "history";
+export default createBrowserHistory();
