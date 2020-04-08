35daeb4d626b91041d8f29499ee40bd42e7adc79
CadiePerkins <56050655+CadiePerkins@users.noreply.github.com>
2020-04-07 18:11:22 -0700

    updates

--- src/account.js
+++ src/account.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import * as bs from 'react-bootstrap';
-import { useAuth0 } from "./react-auth0-spa";
+//import { useAuth0 } from "./react-auth0-spa";
 
 export default function Account(props) {
-    const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
+    // const { isAuthenticated, loginWithRedirect, logout } = useAuth0();
     return (
         <bs.Container fluid className="p-4">
             <bs.Row noGutters>
@@ -15,10 +15,10 @@ export default function Account(props) {
                         generated them, and may be fees involved in handling the item. It also means that the distribution and/or use of 
                         Modifications made by that Participant. If You create or to use it under the GFDL. Thank you Legal Ipsum.</p>
                     <br></br>
-                    {!isAuthenticated && (
+                    {/* {!isAuthenticated && (
                         <bs.Button className="btn btn-primary" onClick={() => loginWithRedirect({})}>Log in</bs.Button>
                     )}
-                    {isAuthenticated && <bs.Button className="btn btn-primary" onClick={() => logout()}>Log out</bs.Button>}
+                    {isAuthenticated && <bs.Button className="btn btn-primary" onClick={() => logout()}>Log out</bs.Button>} */}
                 </bs.Col>
             </bs.Row>
         </bs.Container>
