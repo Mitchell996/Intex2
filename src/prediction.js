8619e4136922f2109a5eb5a8b8326734e281ec7a
Qogcil <jayden.homer@gmail.com>
2020-04-09 17:26:59 -0600

    Add files via upload

--- src/prediction.js
+++ src/prediction.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import * as bs from 'react-bootstrap'
 import { useState } from 'react'
+import AppContext from './context'
 
 export default function Prediction(props){
     const [show, setShow] = useState(true);
+    const context = React.useContext(AppContext)
 
     return (
     <>
         <bs.Alert show={show} variant="success">
-        <bs.Alert.Heading>How Successful Will Your Campaign Be?</bs.Alert.Heading>
+        <bs.Alert.Heading>How Successful is Your Campaign?</bs.Alert.Heading>
         <p>
-            Based on the parameters you provided, your campaign is predicted to reach its goal in <b>[XXX]</b> days. 
+            Based on the parameters you provided, your campaign is predicted to reach its goal in {Math.round(context.currentEstimate)} days. 
         </p>
         <hr />
         <div className="d-flex justify-content-end">
