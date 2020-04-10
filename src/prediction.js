7b61d038951748ba086227ac79ed172f4ed7656d
CadiePerkins <56050655+CadiePerkins@users.noreply.github.com>
2020-04-09 19:03:40 -0700

    one more fix

--- src/prediction.js
+++ src/prediction.js
@@ -22,7 +22,7 @@ export default function Prediction(props){
         </div>
         </bs.Alert>
 
-        {!show && <bs.Button className='my-4 mx-4' onClick={() => setShow(true)}>Show Prediction</bs.Button>}
+        {!show && <bs.Button variant="outline-dark" className='my-4 mx-4' onClick={() => setShow(true)}>Show Prediction</bs.Button>}
     </>
     );
 }
